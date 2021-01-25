4cd6f12397eedd47d8064c9d95119c19b090b12a
Yijia Huang <hyjorc1@gmail.com>
2021-01-25 02:50:28 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <../include/f.h>
+#include "../include/f.h"
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "\nHello world!\n"
-			  << std::endl;
+	cout << "\nHello world!\n"
+		 << endl;
+
+	vector<int> vec{10, 20, 30};
+
+	for (int x : vec)
+		cout << x << " ";
+
+	cout << " " << endl;
+
 	return 0;
 }
