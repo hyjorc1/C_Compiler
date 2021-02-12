fa40c8fde61b146c120d45a853d362f81bded483
Yijia Huang <hyjorc1@gmail.com>
2021-02-12 15:39:00 -0600

    update c lexer

--- lexer-c/mode.c
+++ lexer-c/mode.c
@@ -10,24 +10,50 @@ int main(int argc, char *argv[])
 
     for (int i = 1; i < argc; i++)
     {
+
+        printf("start file %s\n", argv[i]);
         if (newfile(argv[i]))
         {
             int token;
             while ((token = yylex()))
             {
-                if (token == ERR)
+                if (token == END)
                 {
-                    fprintf(stderr, "Error near %s line %d\n", curfilename, err_lineno);
-                    printf("\t%s\n", err_msg);
-                    exit(0);
+                    break;
                 }
                 else
                 {
-                    fprintf(stdout, "%s line %d text '%s' token %s\n", curfilename, lineno(), yytext, map(token));
+                    fprintf(stdout, "%s line %d text '%s' token %s\n", curfilename, yylineno, yytext, map(token));
                 }
             }
         }
     }
+
+    // for (int i = 1; i < argc; i++)
+    // {
+    //     FILE *f = fopen(argv[i], "r");
+    //     if (!f)
+    //     {
+    //         perror(argv[i]);
+    //         return (1);
+    //     }
+    //     yyrestart(f);
+    //     int token;
+    //     while ((token = yylex()))
+    //     {
+    //         if (token == ERR)
+    //         {
+    //             fprintf(stderr, "Error near %s line %d\n", argv[i], err_lineno);
+    //             printf("\t%s\n", err_msg);
+    //             exit(0);
+    //         }
+    //         else
+    //         {
+    //             fprintf(stdout, "%s line %d text '%s' token %s\n", argv[i], yylineno, yytext, map(token));
+    //         }
+    //     }
+    //     fclose(f);
+    // }
 
     return 0;
 }
